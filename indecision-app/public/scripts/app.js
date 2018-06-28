d1399c87698afe3b0edf05306da468fed5894e8f
Carson Kelley <tennisc107@gmail.com>
2018-06-28 18:38:39 -0500

    conditional and example

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -63,7 +63,7 @@ var templateTwo = React.createElement(
         null,
         user.name ? user.name : "Anon"
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         "p",
         null,
         "Age: ",
