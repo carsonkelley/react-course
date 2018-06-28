8049fd10465716b64d20e37139e48638595bf3b2
Carson Kelley <tennisc107@gmail.com>
2018-06-28 06:57:40 -0500

    Added auto-generated app.js file for Babel conversion

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,15 @@
+"use strict";
+
 console.log("App.js is running");
 
 //JSX - JavaScript XML
 
-//var template = <p>This is JSX from app.js</p>;
 var template = React.createElement(
-    "h1",
-    { id: "someid" },
-    "This is JSX from app.js"
-  );
+  "h1",
+  null,
+  "Indecision App"
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
