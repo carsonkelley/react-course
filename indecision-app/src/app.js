79761b136aac9d4569270cde70e8206c530bb07b
Carson Kelley <tennisc107@gmail.com>
2018-06-28 07:44:00 -0500

    Pre-challenge files

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,16 @@ console.log("App.js is running");
 
 //JSX - JavaScript XML
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
 
 var appRoot = document.getElementById("app");
 
