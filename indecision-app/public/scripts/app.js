dc63861783ed6ceaa37ed0c8ad10ec6ea02b7e14
Carson Kelley <tennisc107@gmail.com>
2018-06-29 07:11:57 -0500

    arrow function practice

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function square(x) {
+    return x * x;
+}
+
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+var squareArrow = function squareArrow(x) {
+    return x * x;
+};
+
+console.log(square(8));
+console.log(squareArrow(4));
---
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,14 @@
+function square(x) {
+    return x * x;
+}
+
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+const squareArrow = (x) => x * x;
+
+
+
+console.log(square(8));
+console.log(squareArrow(4));
