a1b14ccece5e9731d4a2b9b4ec8885f5a5da6039
Carson Kelley <tennisc107@gmail.com>
2018-06-29 06:53:17 -0500

    Completed let-const playground file

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,5 +1,32 @@
 "use strict";
 
 var nameVar = "Carson";
-nameVar = "Mike";
+var nameVar = "Mike"; //with var you can redefine variables
 console.log('nameVar', nameVar);
+
+var nameLet = "Janae";
+nameLet = "Julieta";
+console.log("nameLet", nameLet);
+
+var nameConst = "Francoline";
+console.log("nameConst", nameConst);
+
+function getPetName() {
+    var petName = "Hal";
+    return petName;
+}
+
+var petName = getPetName();
+console.log(petName);
+
+//block level scoped
+
+var fullName = "Carson Kelley";
+var firstName = void 0;
+
+if (fullName) {
+    firstName = fullName.split(" ")[0];
+    console.log(firstName);
+}
+
+console.log(firstName);
--- indecision-app/src/playground/es6-let-const.js
+++ indecision-app/src/playground/es6-let-const.js
@@ -1,3 +1,30 @@
 var nameVar = "Carson";
-nameVar = "Mike";
+var nameVar = "Mike"; //with var you can redefine variables
 console.log('nameVar', nameVar);
+
+let nameLet = "Janae";
+nameLet = "Julieta";
+console.log("nameLet", nameLet);
+
+const nameConst = "Francoline";
+console.log("nameConst", nameConst);
+
+function getPetName() {
+    const petName = "Hal";
+    return petName;
+}
+
+const petName = getPetName();
+console.log(petName);
+
+//block level scoped
+
+const fullName = "Carson Kelley";
+let firstName;
+
+if(fullName) {
+    firstName = fullName.split(" ")[0];
+    console.log(firstName);
+}
+
+console.log(firstName);
