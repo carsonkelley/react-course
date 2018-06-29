2d20e60578c940f827ff6e265ee718a624040ec8
Carson Kelley <tennisc107@gmail.com>
2018-06-29 07:37:52 -0500

    Arrow function practice

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,33 @@
 "use strict";
+
+//arguments object - no longer bound with arrow functions
+
+var add = function add(a, b) {
+    //console.log(arguments);
+    return a + b;
+};
+
+console.log(add(55, 1, 1001));
+
+//this keyword - no longer bound
+
+var user = {
+    name: "Carson",
+    cities: ["Houston", "Austin", "Broussard"],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        // console.log(this.name);
+        // console.log(this.cities);
+        // const that = this;
+        return this.cities.map(function (city) {
+            return _this.name + " has lived in " + city;
+        });
+
+        // this.cities.forEach( (city) => {
+        //     console.log(this.name + " has lived in " + city);
+        // });
+    }
+};
+
+console.log(user.printPlacesLived());
---
+++ indecision-app/src/playground/es6-arrow-function-2.js
@@ -0,0 +1,28 @@
+//arguments object - no longer bound with arrow functions
+
+const add =  (a, b) => {
+    //console.log(arguments);
+    return a + b;
+};
+
+console.log(add(55, 1, 1001));
+
+
+//this keyword - no longer bound
+
+const user = {
+    name: "Carson",
+    cities: ["Houston", "Austin", "Broussard"],
+    printPlacesLived() {
+        // console.log(this.name);
+        // console.log(this.cities);
+        // const that = this;
+        return this.cities.map((city) => this.name + " has lived in " + city );
+
+        // this.cities.forEach( (city) => {
+        //     console.log(this.name + " has lived in " + city);
+        // });
+    }
+};
+
+console.log(user.printPlacesLived());
