b93901ca0d21d5c15bfcdd5763acbdfa3e645725
Carson Kelley <tennisc107@gmail.com>
2018-06-29 07:18:09 -0500

    Arrow function practice 1

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,16 +1,25 @@
 "use strict";
 
-function square(x) {
-    return x * x;
-}
-
-// const squareArrow = (x) => {
+// function square(x) {
 //     return x * x;
-// };
+// }
 
-var squareArrow = function squareArrow(x) {
-    return x * x;
+// // const squareArrow = (x) => {
+// //     return x * x;
+// // };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(square(8));
+// console.log(squareArrow(4));
+
+var getFirstName1 = function getFirstName1(fullName) {
+    return fullName.split(" ")[0];
 };
 
-console.log(square(8));
-console.log(squareArrow(4));
+var getFirstName = function getFirstName(fullName) {
+    return fullName.split(" ")[0];
+};
+
+console.log(getFirstName1("John Hortenburg"));
+console.log(getFirstName("Carson Kelley"));
--- indecision-app/src/playground/es6-arrow-function.js
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -1,14 +1,21 @@
-function square(x) {
-    return x * x;
-}
-
-// const squareArrow = (x) => {
+// function square(x) {
 //     return x * x;
-// };
+// }
 
-const squareArrow = (x) => x * x;
+// // const squareArrow = (x) => {
+// //     return x * x;
+// // };
 
+// const squareArrow = (x) => x * x;
 
+// console.log(square(8));
+// console.log(squareArrow(4));
 
-console.log(square(8));
-console.log(squareArrow(4));
+const getFirstName1 = (fullName) => {
+    return fullName.split(" ")[0];
+};
+
+const getFirstName = (fullName) => fullName.split(" ")[0];
+
+console.log(getFirstName1("John Hortenburg"));
+console.log(getFirstName("Carson Kelley"));
