66ae426a96546f0bfa6b910fb0213ffe833388b8
Carson Kelley <tennisc107@gmail.com>
2018-07-02 17:15:29 -0500

    Pre-challenge button functionality

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -44,7 +44,8 @@ var template = React.createElement(
 
 var count = 0;
 var addOne = function addOne() {
-    console.log("add one");
+    count++;
+    renderCounterApp();
 };
 var minusOne = function minusOne() {
     console.log("minus one");
@@ -52,32 +53,37 @@ var minusOne = function minusOne() {
 var reset = function reset() {
     console.log("reset");
 };
-var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        "Count: ",
-        count
-    ),
-    React.createElement(
-        "button",
-        { onClick: addOne },
-        "+1"
-    ),
-    React.createElement(
-        "button",
-        { onClick: minusOne },
-        "-1"
-    ),
-    React.createElement(
-        "button",
-        { onClick: reset },
-        "Reset"
-    )
-);
-console.log(templateTwo);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Count: ",
+            count
+        ),
+        React.createElement(
+            "button",
+            { onClick: addOne },
+            "+1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: minusOne },
+            "-1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: reset },
+            "Reset"
+        )
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
