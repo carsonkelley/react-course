f8b51c918cd0bd8c88be0d852cabee756c79c586
Carson Kelley <tennisc107@gmail.com>
2018-07-03 12:14:32 -0500

    Array items challenge

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -25,6 +25,8 @@ const onRemoveAll = () => {
 
 const appRoot = document.getElementById("app");
 
+const numbers = [55, 101, 1000];
+
 const render = () => {
     const template = (
         <div>
@@ -34,8 +36,9 @@ const render = () => {
             <p>{app.options.length}</p>
             <button onClick={onRemoveAll}>Remove all</button>
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+                {app.options.map((option, index) => {
+                    return <li key={index}>{option}</li>;
+                })}
             </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option"/>
