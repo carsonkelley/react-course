fcef7082ebcbb56dd8725dde11dee8a254bafec8
Carson Kelley <tennisc107@gmail.com>
2018-07-09 07:31:18 -0500

    Converted visibility toggle to React Component

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -30,6 +30,8 @@ const render = () => {
 };
 
 render(); */
+
+/*
 let visibility = false;
 
 const toggleButton = () => {
@@ -48,4 +50,32 @@ const render = () => {
     ReactDOM.render(jsx, document.getElementById("app"));
 };
 
-render();
+render(); */
+
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? "Hide Details" : "Show Details"}</button>
+                {this.state.visibility && <p>These are the details that we're talking about</p>}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
