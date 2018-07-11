76a6cc737238c15a6b8db415bebf1fb884490966
Carson Kelley <tennisc107@gmail.com>
2018-07-11 07:03:34 -0500

    Converted set state to simpler arrow function

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -9,11 +9,7 @@ class IndecisionApp extends React.Component {
         };
     }
     handleDeleteOptions() {
-        this.setState(() => {
-            return {
-                options: []
-            }
-        });
+        this.setState(() => ({ options: [] }));
     }
     handlePick() {
         const randomNumber = Math.floor(Math.random() * this.state.options.length);
@@ -26,11 +22,8 @@ class IndecisionApp extends React.Component {
         }else if(this.state.options.indexOf(option) > -1) {
             return 'This option already exists';
         } 
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat(option)
-            };
-        });
+        this.setState((prevState) => ( {options: prevState.options.concat(option) }));
+
     }
     render() {
         const subtitle = "Put your life in the hands of a computer.";
@@ -115,9 +108,7 @@ class AddOption extends React.Component {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
-        this.setState(() => {
-            return { error };
-        });
+        this.setState(() => ({ error }));
     }
     render() {
         return (
@@ -131,14 +122,5 @@ class AddOption extends React.Component {
         );
     }
 }
-
-// const User = (props) => {
-//     return (
-//         <div>
-//             <p>Name: {props.name}</p>
-//             <p>Age: {props.age}</p>
-//         </div>
-//     );
-// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
