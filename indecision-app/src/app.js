f5eaa9a5ac74e9304017e9ab9b4efef85c380271
Carson Kelley <tennisc107@gmail.com>
2018-07-13 06:58:46 -0500

    Saving options to localStorage

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -10,10 +10,24 @@ class IndecisionApp extends React.Component {
         };
     }
     componentDidMount() {
-        console.log("component did mount");
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({ options }));
+            }
+         } catch (e) {
+
+        }
+        
     }
     componentDidUpdate(prevProps, prevState) {
-        console.log("component did update");
+        if(prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+            console.log("saving data");
+        }
     }
     componentWillUnmount() {
         console.log("component will unmount");
@@ -96,6 +110,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => (
                     <Option 
@@ -138,6 +153,10 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
         this.setState(() => ({ error }));
+
+        if(!error) {
+            e.target.elements.option.value = '';
+        }
     }
     render() {
         return (
