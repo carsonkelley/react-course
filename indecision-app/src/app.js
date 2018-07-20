8b49a6fe8a45c405041e5ee1262d99ffaefed236
Carson Kelley <tennisc107@gmail.com>
2018-07-20 07:23:00 -0500

    Importing React and ReactDOM with npm modules

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,5 @@
-//import './utils.js';
-// import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log("app.js is running!");
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 19));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(14));
-console.log(canDrink(55));
-console.log(isSenior(67));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
