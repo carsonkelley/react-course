082c09ef7c86c31d1f489874462c1e174fbc68a2
Carson Kelley <tennisc107@gmail.com>
2018-08-06 17:19:44 -0500

    Array destructuring examples

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //     name: 'Carson',
 //     age: 26,
@@ -16,14 +20,30 @@
 //     console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-    title: 'The Brothers Karamazov',
-    author: 'Fyodor Dostoevsky',
-    publisher: {
-        name: 'Farrar, Straus and Giroux'
-    }
-};
 
-const { name: publisherName = 'Self-Published'} = book.publisher;
+// const book = {
+//     title: 'The Brothers Karamazov',
+//     author: 'Fyodor Dostoevsky',
+//     publisher: {
+//         name: 'Farrar, Straus and Giroux'
+//     }
+// };
 
-console.log(publisherName); //valid publisher name, or self-published if nothing
+// const { name: publisherName = 'Self-Published'} = book.publisher;
+
+// console.log(publisherName); //valid publisher name, or self-published if nothing
+
+//
+// Array destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'StateNotFound'] = address;
+// console.log(`You are in ${city}, ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$.75'];
+
+const [menuItem, , mediumPrice] = item;
+
+//grab first and third item ussing destructuring
+console.log(`A medium ${menuItem} costs ${mediumPrice}`);
