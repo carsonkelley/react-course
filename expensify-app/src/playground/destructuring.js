f2895bcb614f48eba90d2bffb325d481bba98757
Carson Kelley <tennisc107@gmail.com>
2018-08-06 17:06:48 -0500

    Destructuring challenge, book publisher

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,18 +1,29 @@
-const person = {
-    name: 'Carson',
-    age: 26,
-    location: {
-        city: 'Houston',
-        temp: 93
+// const person = {
+//     name: 'Carson',
+//     age: 26,
+//     location: {
+//         city: 'Houston',
+//         temp: 93
+//     }
+// };
+
+// const {name:firstName = 'Anonymous', age} = person;
+// console.log(`${firstName} is ${age}.`);
+
+
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
+
+const book = {
+    title: 'The Brothers Karamazov',
+    author: 'Fyodor Dostoevsky',
+    publisher: {
+        name: 'Farrar, Straus and Giroux'
     }
 };
 
-const {name:firstName = 'Anonymous', age} = person;
-console.log(`${firstName} is ${age}.`);
+const { name: publisherName = 'Self-Published'} = book.publisher;
 
-
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}.`);
-}
-
+console.log(publisherName); //valid publisher name, or self-published if nothing
