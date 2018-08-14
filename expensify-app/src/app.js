eb5fd61dcaf85da7ee9b6689a64faacd7ad10db3
Carson Kelley <tennisc107@gmail.com>
2018-08-14 07:59:44 -0500

    Added react-redux, created ExpenseList with connect react-redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
 
 
 const store = configureStore();
@@ -15,10 +17,20 @@ store.dispatch(addExpense({ description: 'Water Bill', amount: 35 }));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 57 }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000);
+
 const state= store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
